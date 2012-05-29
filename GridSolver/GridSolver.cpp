be24542c6e6ad6afa2aae933a3e5406745116fff
Brian Lee <thedragonkaiser@gmail.com>
2012-05-29 12:05:24 +0900

    mode

--- GridSolver/GridSolver.cpp
+++ GridSolver/GridSolver.cpp
@@ -39,6 +39,15 @@ struct Config {
 	int nPlot;
 	time_t tDuration;
 
+	int nMode;
+
+	enum {
+		HYBRID_GA = 0,
+		LOCAL_OP,
+		MULTI_START,
+		PURE_GA,
+	};
+
 	Config() {};
 	Config(CCmdLine cmdLine) {
 		sInput = ( cmdLine.GetArgument("-i", 0) );
@@ -48,6 +57,8 @@ struct Config {
 		bPlot = cmdLine.HasSwitch("-plot");
 		nPlot = bPlot ? Utility::strTo<int>( cmdLine.GetArgument("-plot", 0) ) : 0;
 		tDuration = cmdLine.HasSwitch("-t") ? Utility::strTo<int>( cmdLine.GetArgument("-t", 0) ) : 360000 - 2500;
+
+		nMode = Utility::strTo<int>( cmdLine.GetArgument("-mode", 0) );
 	}
 };
 
@@ -77,114 +88,154 @@ int main(int argc, char* argv[]) {
 	generate_n(back_inserter(population), config.nPopulation, genSolution);
 	sort(population.begin(), population.end(), GA::SolutionPtrGreater());
 	
-	if (config.bPlot) {
-		cout << "Gen\tSize\tBest\tMed\tWorst\t"
-			<< "CostAvg\tCostSTD\t"
-			<< "SelAvg\tSelSTD\t"
-			<< "DiffAvg\tDiffSTD\t"
-			<< "XEffAvg\tXEffSTD"
-			<< endl;
-		cout.precision(4);
-	}
-
 	////////////////////////////////////////////////////////
 	//////// main Loop
 	GA::GAHelper ga(cmdLine);
 	int nGeneration = 0;
-	int k = (int)(config.nPopulation * config.fGenerationGap);
-	if ( k < 1 ) k = 1;
-	while (true) {
-		GA::Solution::Vector offsprings;
-		offsprings.reserve(k);
 
-		vector<GA::Solution::Pair> parentsVec;
-		parentsVec.reserve(k);
-
-		for (int i=0; i<k; ++i) {
-			if (i==0)
-				ga.generateFitness(population);
-			GA::Solution::Pair parents = ga.select(population);
-			parentsVec.push_back(parents);
-
-			GA::Solution::Ptr pOffspring = ga.crossover(parents);
-//			GA::Solution::Ptr pOffspring = parents.first;
-
-			ga.mutate(pOffspring, g_pGridHelper);
-
-			pOffspring->cost = g_pGridHelper->scoreGrid(pOffspring);
-			g_pGridHelper->localOptimization(pOffspring);
-
-			offsprings.push_back(pOffspring);
+	switch (config.nMode) {
+	case Config::HYBRID_GA: {
+		if (config.bPlot) {
+			cout << "Gen\tSize\tBest\tMed\tWorst\t"
+			<< "CostAvg\tCostSTD\t"
+			<< "DiffAvg\tDiffSTD\t"
+			<< endl;
+			cout.precision(4);
 		}
-		ga.replace(offsprings, parentsVec, population);
 
-		++nGeneration;
-		////////////////////////////////////////////////////////
-		//////// plot
-		if (config.bPlot && nGeneration % config.nPlot == 0) {
-			GA::Solution::Ptr pBest = population.front();
-			GA::Solution::Ptr pMedian = population[population.size() / 2];
-			GA::Solution::Ptr pWorst = population.back();
+		int k = (int)(config.nPopulation * config.fGenerationGap);
+		if ( k < 1 ) k = 1;
+		while (true) {
+			GA::Solution::Vector offsprings;
+			offsprings.reserve(k);
 
-			long long nCostSum = 0;
-			vector<int> solutionDiff;
-			solutionDiff.reserve(population.size() * population.size());
-			GA::Solution::Vector::iterator it = population.begin();
-			for (; it != population.end(); ++it) {
-				nCostSum += (*it)->cost;
-				GA::Solution::Vector::iterator kit = population.begin();
-				for (; kit != population.end(); ++kit)
-					solutionDiff.push_back((*it)->getDistance(*kit));
+			vector<GA::Solution::Pair> parentsVec;
+			parentsVec.reserve(k);
+
+			for (int i=0; i<k; ++i) {
+				if (i==0)
+					ga.generateFitness(population);
+				GA::Solution::Pair parents = ga.select(population);
+				parentsVec.push_back(parents);
+
+				GA::Solution::Ptr pOffspring = ga.crossover(parents);
+
+				ga.mutate(pOffspring, g_pGridHelper);
+
+				pOffspring->cost = g_pGridHelper->scoreGrid(pOffspring);
+				g_pGridHelper->localOptimization(pOffspring);
+
+				offsprings.push_back(pOffspring);
+			}
+			ga.replace(offsprings, parentsVec, population);
+
+			++nGeneration;
+
+			////////////////////////////////////////////////////////
+			//////// plot
+			if (config.bPlot && nGeneration % config.nPlot == 0) {
+				GA::Solution::Ptr pBest = population.front();
+				GA::Solution::Ptr pMedian = population[population.size() / 2];
+				GA::Solution::Ptr pWorst = population.back();
+
+				long long nCostSum = 0;
+				vector<int> solutionDiff;
+				solutionDiff.reserve(population.size() * population.size());
+				GA::Solution::Vector::iterator it = population.begin();
+				for (; it != population.end(); ++it) {
+					nCostSum += (*it)->cost;
+					GA::Solution::Vector::iterator kit = population.begin();
+					for (; kit != population.end(); ++kit)
+						solutionDiff.push_back((*it)->getDistance(*kit));
+				}
+
+				float fCostAvg = (float)nCostSum / population.size();
+				float fCostDev = 0;
+				it = population.begin();
+				for (; it != population.end(); ++it)
+					fCostDev += (((*it)->cost - fCostAvg) * ((*it)->cost - fCostAvg));
+
+				float fDiffAvg = accumulate(solutionDiff.begin(), solutionDiff.end(), 0) / solutionDiff.size();
+				float fDiffDev = 0;
+				for (int i=0; i<solutionDiff.size(); ++i)
+					fDiffDev += ((solutionDiff[i]- fDiffAvg) * (solutionDiff[i]- fDiffAvg));
+
+				cout << nGeneration << "\t" << population.size() << "\t" <<
+					pBest->cost << "\t" << pMedian->cost << "\t" << pWorst->cost << "\t"
+					<< fixed
+					<< fCostAvg << "\t" << sqrt(fCostDev / population.size()) << "\t" 
+					<< fDiffAvg << "\t" << sqrt(fDiffDev / solutionDiff.size()) << "\t"
+					<< endl;
 			}
 
-			long long nSelCostSum = 0;
-			for (int i=0; i<parentsVec.size(); ++i) {
-				nSelCostSum += parentsVec[i].first->cost;
-				nSelCostSum += parentsVec[i].second->cost;
+			tCurrent = Utility::getMilliSec();
+			if (tCurrent > tEnd)
+				break;
+		}
+	}
+	break;
+
+	case Config::PURE_GA: {
+		int k = (int)(config.nPopulation * config.fGenerationGap);
+		if ( k < 1 ) k = 1;
+		while (true) {
+			GA::Solution::Vector offsprings;
+			offsprings.reserve(k);
+
+			vector<GA::Solution::Pair> parentsVec;
+			parentsVec.reserve(k);
+
+			for (int i=0; i<k; ++i) {
+				if (i==0)
+					ga.generateFitness(population);
+				GA::Solution::Pair parents = ga.select(population);
+				parentsVec.push_back(parents);
+
+				GA::Solution::Ptr pOffspring = ga.crossover(parents);
+				ga.mutate(pOffspring, g_pGridHelper);
+				pOffspring->cost = g_pGridHelper->scoreGrid(pOffspring);
+				offsprings.push_back(pOffspring);
 			}
+			ga.replace(offsprings, parentsVec, population);
 
-			vector<int> crossEffects;
-			solutionDiff.reserve(offsprings.size());
-			for (int i=0; i<offsprings.size(); ++i) {
-				int effect = offsprings[i]->getDistance(parentsVec[i].first) + offsprings[i]->getDistance(parentsVec[i].second);
-				crossEffects.push_back( effect / 2 );
-			}
+			++nGeneration;
+			tCurrent = Utility::getMilliSec();
+			if (tCurrent > tEnd)
+				break;
+		}
+	}
+	break;
 
-			float fCostAvg = (float)nCostSum / population.size();
-			float fCostDev = 0;
-			it = population.begin();
-			for (; it != population.end(); ++it)
-				fCostDev += pow(((*it)->cost - fCostAvg), 2);
+	case Config::LOCAL_OP: {
+		population.clear();
 
-			float fSelCostAvg = (float)nSelCostSum / (parentsVec.size() * 2);
-			float fSelCostDev = 0;
-			for (int i=0; i<parentsVec.size(); ++i) {
-				fSelCostDev += pow((parentsVec[i].first->cost - fSelCostAvg), 2);
-				fSelCostDev += pow((parentsVec[i].second->cost - fSelCostAvg), 2);
-			}
+		for (int i=0; i<100; ++i) {
+			GA::Solution::Ptr pSol = genSolution();
+			pSol->cost = g_pGridHelper->scoreGrid(pSol);
+			g_pGridHelper->localOptimization(pSol);
+			population.push_back(pSol);
+		}
+	}
+	break;
 
-			float fDiffAvg = accumulate(solutionDiff.begin(), solutionDiff.end(), 0) / solutionDiff.size();
-			float fDiffDev = 0;
-			for (int i=0; i<solutionDiff.size(); ++i)
-				fDiffDev += pow((solutionDiff[i]- fDiffAvg) , 2);
+	case Config::MULTI_START: {
+		population.clear();
+		population.push_back( genSolution() );
+		while (true) {
+			GA::Solution::Ptr pSol = genSolution();
+			pSol->cost = g_pGridHelper->scoreGrid(pSol);
+			g_pGridHelper->localOptimization(pSol);
 
-			float fXEffectAvg = accumulate(crossEffects.begin(), crossEffects.end(), 0) / crossEffects.size();
-			float fXEffectDev = 0;
-			for (int i=0; i<crossEffects.size(); ++i)
-				fXEffectDev += pow((crossEffects[i]- fXEffectAvg) , 2);
+			if ( pSol->cost > population[0]->cost )
+				population[0] = pSol;
 			
-			cout << nGeneration << "\t" << population.size() << "\t" <<
-				pBest->cost << "\t" << pMedian->cost << "\t" << pWorst->cost << "\t"
-				<< fixed
-				<< fCostAvg << "\t" << sqrt(fCostDev / population.size()) << "\t" 
-				<< fSelCostAvg << "\t" << sqrt(fSelCostDev / (parentsVec.size() * 2)) << "\t" 
-				<< fDiffAvg << "\t" << sqrt(fDiffDev / solutionDiff.size()) << "\t"
-				<< fXEffectAvg << "\t" << sqrt(fXEffectDev / crossEffects.size()) << endl;
+			tCurrent = Utility::getMilliSec();
+			if (tCurrent > tEnd)
+				break;
 		}
+	}
+	break;
 
-		tCurrent = Utility::getMilliSec();
-		if (tCurrent > tEnd)
-			break;
 	}
 
 	////////////////////////////////////////////////////////
@@ -196,25 +247,42 @@ int main(int argc, char* argv[]) {
 	fout.open(config.sOutput, fstream::out | fstream::trunc);
 #endif
 	if (fout.is_open()) {
-		GA::Solution::Ptr pSol = population.front();
-		int size = (int)pSol->genotype.size();
-		for (int i=0; i<size; ++i) {
-			if ( (i+1) % pSol->width == 0)
-				fout << pSol->genotype[i] << endl;
-			else
-				fout << pSol->genotype[i] << " ";
+		if (config.nMode == Config::LOCAL_OP) {
+			long long nCostSum = 0;
+			GA::Solution::Vector::iterator it = population.begin();
+			for (; it != population.end(); ++it)
+				nCostSum += (*it)->cost;
+
+			float fCostAvg = (float)nCostSum / population.size();
+
+			float fCostDev = 0;
+			it = population.begin();
+			for (; it != population.end(); ++it)
+				fCostDev += (((*it)->cost - fCostAvg) * ((*it)->cost - fCostAvg));
+			fout << fCostAvg << "\t" << sqrt(fCostDev / population.size()) << "\t" << endl;
+		}
+		else {
+			GA::Solution::Ptr pSol = population.front();
+			int size = (int)pSol->genotype.size();
+			for (int i=0; i<size; ++i) {
+				if ( (i+1) % pSol->width == 0)
+					fout << pSol->genotype[i] << endl;
+				else
+					fout << pSol->genotype[i] << " ";
+			}
+
+	#if defined (_TEST_)
+			fout << endl;
+			fout << "Generations: " << nGeneration << endl;
+			fout << "Score : " << pSol->cost << endl;
+			for (int i=0; i<argc; ++i)
+				fout << argv[i] << " ";
+			fout << endl;
+			fout << "================================================================================" << endl;
+			fout << endl;
+	#endif
 		}
 
-#if defined (_TEST_)
-		fout << endl;
-		fout << "Generations: " << nGeneration << endl;
-		fout << "Score : " << pSol->cost << endl;
-		for (int i=0; i<argc; ++i)
-			fout << argv[i] << " ";
-		fout << endl;
-		fout << "================================================================================" << endl;
-		fout << endl;
-#endif
 		fout.close();
 	}
 
